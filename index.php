d5d20a5a56ba849860f24092aa1de3e5e1a7720f
AIMAN070 <155106996+AIMAN070@users.noreply.github.com>
2025-05-01 02:22:55 +0500

    Update index.php

--- index.php
+++ index.php
@@ -14,33 +14,20 @@ include 'db_config.php';
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
-  <div class="container">
-    <h1>Welcome to Blood Bank Management System</h1>
-    <nav>
-      <a href="add_donor.php">Add Donor</a>
-      <a href="add_patient.php">Add Patient</a>
-      <a href="add_doctor.php">Add Doctor</a>
-      <a href="add_blood_stock.php">Update Blood Stock</a>
-      <a href="search.php">Search by Blood</a>
-      <a href="logout.php">Logout</a>
-
-    </nav>
-
-    <h2>Available Blood Stock</h2>
-    <table>
-      <tr><th>Blood Group</th><th>Units Available</th></tr>
-      <?php
-      $sql    = "SELECT blood_group, quantity FROM blood_stock";
-      $result = $conn->query($sql);
-      if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-          echo "<tr><td>{$row['blood_group']}</td><td>{$row['quantity']}</td></tr>";
-        }
-      } else {
-        echo "<tr><td colspan='2'>No data available</td></tr>";
-      }
-      ?>
-    </table>
+<div class="container">
+  <h1>
+    Welcome<br> Blood Bank Management System
+  </h1>
+  <div class="big-box">
+  <ul class="main-menu">
+    <li><a href="add_patient.php">Add Patient</a></li>
+    <li><a href="add_doctor.php">Add Doctor</a></li>
+    <li><a href="add_donor.php">Add Donor</a></li>
+    <li><a href="add_blood_stock.php">Update Blood Stock</a></li>
+    <li><a href="add_available_blood_stock.php">Available Blood Stock</a></li>
+    <li><a href="search.php">Search by Blood</a></li>
+    <li><a href="logout.php">Logout</a></li>
+  </ul>
   </div>
 </body>
 </html>
